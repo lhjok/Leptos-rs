ae7f3b9b3fb7fd7233275df5e1da6b130ae126f1
lhjok <lhjok@live.cn>
2023-09-26 03:24:59 +0800

    Update wrong.rs

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,14 +1,14 @@
 pub mod pages;
 pub mod home;
 pub mod login;
-pub mod login_err;
+pub mod wrong;
 pub mod signup;
 pub mod admin;
 pub mod error;
 
 pub use self::home::Home;
 pub use self::login::Login;
-pub use self::login_err::LoginErr;
+pub use self::wrong::LoginErr;
 pub use self::signup::Signup;
 pub use self::admin::Admin;
 pub use self::error::Error;
