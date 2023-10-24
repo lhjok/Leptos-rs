b40452d8592d40d0108db2fe06b95995f5360fbf
lhjok <lhjok@live.cn>
2023-10-24 19:38:19 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,10 +5,10 @@ pub fn main() {
     println!("cargo:rerun-if-changed=Cargo.lock");
 
     let root_dir: std::path::PathBuf = std::env::var("CARGO_MANIFEST_DIR").unwrap().into();
-    let generated_dir = root_dir.join("static");
-    let js_dir = generated_dir.join("scripts");
+    let static_dir = root_dir.join("static");
+    let js_dir = static_dir.join("scripts");
 
-    leptonic_theme::generate(generated_dir.join("leptonic"));
+    leptonic_theme::generate(static_dir.join("leptonic"));
     println!("cargo:warning=theme written");
 
     std::fs::create_dir_all(js_dir.clone()).unwrap();
